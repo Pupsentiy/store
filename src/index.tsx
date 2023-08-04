c3c93990edc682895d2d331a67aecfcbfffbd446
Pupsentiy <pupsentiy@gmail.com>
2023-08-04 11:35:24 +0300

    add redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './app/App'
-import { ThemeProvider } from 'app/providers/ThemeProviders'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
+import { ThemeProvider } from 'app/providers/ThemeProviders'
+import { StoreProvider } from 'app/providers/StoreProvider'
+import App from './app/App'
 import 'app/styles/index.scss'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement as Element)
 
 root.render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>
 )
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,17 +1,23 @@
 import { type ReactNode } from 'react'
 import { render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import { StoreProvider } from 'app/providers/StoreProvider'
+import { type DeepPartial } from '@reduxjs/toolkit'
+import { type StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
 
 export interface componentRenderOptions {
   route?: string
+  initialState?: DeepPartial<StateSchema>
 }
 
 export function componentRender (
   component: ReactNode,
   options: componentRenderOptions = {}
 ) {
-  const { route = '/' } = options
+  const { route = '/', initialState } = options
   return render(
-    <MemoryRouter initialEntries={[route]}>{component}</MemoryRouter>
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>{component}</MemoryRouter>
+    </StoreProvider>
   )
 }
