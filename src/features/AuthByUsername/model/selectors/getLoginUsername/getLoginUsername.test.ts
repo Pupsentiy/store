3f4b7d6954ddc4b20f1526087dfee2cd5369b787
Pupsentiy <pupsentiy@gmail.com>
2023-08-15 14:56:09 +0300

    fix test getLoginUsername

--- src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
+++ src/features/AuthByUsername/model/selectors/getLoginUsername/getLoginUsername.test.ts
@@ -6,12 +6,11 @@ describe('getLoginUsername.test', () => {
   test('should return value', () => {
     const state: DeepPartial<StateSchema> = {
       loginForm: {
-        password: '123123'
+        username: '123123'
       }
     }
     expect(getLoginUsername(state as StateSchema)).toEqual('123123')
   })
-
   test('should work with empty state', () => {
     const state: DeepPartial<StateSchema> = {}
     expect(getLoginUsername(state as StateSchema)).toEqual('')
