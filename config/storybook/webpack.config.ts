3ebae474433085ea107ad7b0ddb912ec0561c506
Pupsentiy <pupsentiy@gmail.com>
2023-08-24 15:42:29 +0300

    fix typings

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,23 +10,26 @@ export default ({ config }: { config: webpack.Configuration }) => {
     entry: "",
     src: path.resolve(__dirname, "..", "..", "src"),
   };
-  config.resolve.modules.push(paths.src);
-  config.resolve.extensions.push(".ts", ".tsx");
+  config!.resolve!.modules!.push(paths.src);
+  config!.resolve!.extensions!.push(".ts", ".tsx");
 
-  config.module.rules = config.module.rules.map((rule: webpack.RuleSetRule) => {
-    if (/svg/.test(rule.test as string)) {
-      return { ...rule, exclude: /\.svg$/i };
+  config!.module!.rules = config!.module!.rules!.map(
+    // @ts-ignore
+    (rule: webpack.RuleSetRule) => {
+      if (/svg/.test(rule.test as string)) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+
+      return rule;
     }
+  );
 
-    return rule;
-  });
-
-  config.module.rules.push({
+  config!.module!.rules!.push({
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   });
-  config.module.rules.push(buildCssLoader(true));
-  config.plugins.push(
+  config!.module!.rules.push(buildCssLoader(true));
+  config!.plugins!.push(
     new webpack.DefinePlugin({
       _IS_DEV_: JSON.stringify(true),
       _API_: JSON.stringify(""),
